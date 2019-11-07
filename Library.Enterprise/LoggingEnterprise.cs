43ba4d5b49b46e4ed11d51eff263aa08cd6f0dc0
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-07 08:14:05 +0100

    Add class LoggingEnterprise

---
+++ Library.Enterprise/LoggingEnterprise.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
+
+namespace Library.Enterprise
+{
+    public class LoggingEnterprise
+    {
+        private readonly IConfigurationSource configurationSource = ConfigurationSourceFactory.Create();
+
+        public void LogIn(System.Diagnostics.TraceEventType traceEventType, string logMessage)
+        {
+            LogWriterFactory logWriterFactory = new LogWriterFactory(configurationSource);
+            Logger.SetLogWriter(logWriterFactory.Create());
+            if (!Logger.IsLoggingEnabled())
+            {
+                return;
+            }
+            Logger.Writer.Write(new LogEntry()
+            {
+                Severity = traceEventType,
+                TimeStamp = DateTime.Now,
+                Message = traceEventType + " " + logMessage
+            });
+
+        }
+    }
+}
